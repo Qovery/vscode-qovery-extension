722c879e75dda093f7708f5c09fc0b2e3eb6f60d
alexisvisco <alexis.viscogliosi@outlook.fr>
2020-04-02 20:36:57 +0200

    initial: commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# vscode-qovery-plugin
-Vscode plugin with auto completion
+# Qovery Extension
+Qovery Extension makes it easy to edit Qovery yaml specification files inside vscode.
+
+## Demo 
+![Demo of the plugin](.github/ext-demo.gif)
+
+## Usage
+Open a file that end with .qovery.yml :)
+
+## Compatibility
+This extension support vscode at version `^1.25.0`.
+
+## Dependency
+
+This plugin use the [YAML](https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml) extension created by Redhat.
+
+## Contribution
+
+Author: [Alexis Viscogliosi](https://github.com/alexisvisco)
---
+++ src/extension.ts
@@ -0,0 +1,61 @@
+import { ConfigurationTarget, Extension, extensions, Uri, window, workspace } from 'vscode'
+import { join } from 'path'
+
+// force QoverySpec to be included
+import './domain/QoverySpec.json'
+
+const YAML_SCHEMA_CONFIG_NAME_OF_VSCODE_YAML_EXTENSION = 'yaml.schemas'
+const VSCODE_YAML_EXTENSION_ID = 'redhat.vscode-yaml'
+const QOVERY_FILE_PATTERN = '*.qovery.y*ml'
+const SCHEMA_FILE = Uri.file(join(__dirname, `/domain/QoverySpec.json`)).toString()
+
+export const activate = async () => {
+  await addQoverySchemaToConfig()
+  await activateYamlExtension()
+}
+
+export const deactivate = () => {}
+
+const activateYamlExtension = async () => {
+  const ext: Extension<any> | undefined = extensions.getExtension(VSCODE_YAML_EXTENSION_ID)
+  if (!ext) {
+    window.showWarningMessage('Please install \'YAML Support by Red Hat\' via the Extensions pane.')
+    return
+  }
+
+  const yamlPlugin = await ext.activate()
+
+  console.log(yamlPlugin)
+
+  if (!yamlPlugin) {
+    window.showWarningMessage(' Please upgrade \'YAML Support by Red Hat\' via the Extensions pane.')
+    return
+  }
+  return yamlPlugin
+}
+
+const addQoverySchemaToConfig = async () => {
+  const config = workspace.getConfiguration().inspect(YAML_SCHEMA_CONFIG_NAME_OF_VSCODE_YAML_EXTENSION)
+
+  if (config !== undefined) {
+    await addQoverySchemaToConfigAtScope(SCHEMA_FILE, QOVERY_FILE_PATTERN, ConfigurationTarget.Global, config.globalValue)
+  }
+}
+
+const addQoverySchemaToConfigAtScope = async (key: string, value: string, scope: ConfigurationTarget, valueAtScope: any) => {
+  let newValue: any = {}
+  if (valueAtScope) {
+    newValue = Object.assign({}, valueAtScope)
+  }
+
+  Object.keys(newValue).forEach(configKey => {
+    var configValue = newValue[configKey]
+    if (value === configValue) {
+      delete newValue[configKey]
+    }
+  })
+
+  newValue[key] = value
+
+  await workspace.getConfiguration().update(YAML_SCHEMA_CONFIG_NAME_OF_VSCODE_YAML_EXTENSION, newValue, scope)
+}
